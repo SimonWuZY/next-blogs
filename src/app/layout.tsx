c1bc2bb576d79237d9486c22ea41b81795225f2c
Simon_Wzy <1691663479@qq.com>
2024-11-20 13:59:47 +0800

    header修改

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
+        {/* FIXME: 顶部栏目始终出现 */}
         <Header />
         <div className="flex-grow p-4 sm:p-6 lg:p-8">
           <div className="container mx-auto">
