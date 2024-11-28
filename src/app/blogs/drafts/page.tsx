4b9ec92b560c6854b03c97a11961a549607cdbe5
Simon_Wzy <1691663479@qq.com>
2024-11-28 21:08:15 +0800

    TODO： apps drafts blogs keep going forward

---
+++ src/app/blogs/drafts/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
+import './style.css';
+export default function Drafts() {
+    const [value, setValue] = useState('');
+    console.log(value);
+
+    return (
+        <div className='h-screen markdown-body'>
+            {/* 标题部分 */}
+            <div className='flex flex-col justify-center items-center px-2 mb-5'>
+                <div className='text-blue-600 text-[50px] font-extrabold'>开始创作</div>
+                <p className='text-blue-400 text[30px] font-medium'>享受记录与分享趣事知识的快乐</p>
+            </div>
+
+            {/* 富文本编辑器部分，设置其高度为 100% */}
+            <div className='h-full'>
+                <ReactQuill theme="snow" value={value} onChange={setValue} style={{ height: '100%' }} />
+            </div>
+        </div>
+    );
+}
--- src/app/components/BlogFooter.tsx
+++ src/app/components/BlogFooter.tsx
@@ -1,8 +1,13 @@
 
+import Button from 'antd/es/button'
+import { useRouter } from "next/navigation"
 import React from 'react'
 
-export default function BlogFooter()  {
+export default function BlogFooter() {
+  const router = useRouter()
   return (
-    <div>BlogFooter</div>
+    <div>
+      <Button type="primary" onClick={() => router.push('/drafts')}>Primary Button</Button>
+    </div>
   )
 }
